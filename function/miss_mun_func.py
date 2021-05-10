ad85efeb329984d3cbd0cc6bf6e5728486c39c6c
Chien-Ming Lo <mason9601012@gmail.com>
2021-05-10 14:24:23 +0800

    revise the function

--- function/miss_mun_func.py
+++ function/miss_mun_func.py
@@ -1,3 +1,4 @@
-
-def miss_num_func(lst):
-    return sorted(set(range(lst[0], lst[-1])) - set(lst))
+def miss_num_func2(lst):
+    miss=sorted(set(range(lst[0], lst[-1])) - set(lst))
+    for i in miss:
+        print(i)
